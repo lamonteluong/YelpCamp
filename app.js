5c2c954733b5301a554b8afd3fd77bea830e3f0e
lluong <lamonte.luong@gmail.com>
2019-06-12 02:19:41 -0400

    Added MongoDB capabilities for data persistence

--- app.js
+++ app.js
@@ -1,43 +1,73 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp-camp", {useNewUrlParser: true});
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campArr = [
-    {name: "Salmon Creek", image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg'},
-    {name: 'Green Hills', image: 'https://farm4.staticflickr.com/3282/2770447094_2c64348643.jpg'},
-    {name: 'Big Peak', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg'},
-    {name: "Salmon Creek", image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg'},
-    {name: 'Green Hills', image: 'https://farm4.staticflickr.com/3282/2770447094_2c64348643.jpg'},
-    {name: 'Big Peak', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg'},
-    {name: "Salmon Creek", image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg'},
-    {name: 'Green Hills', image: 'https://farm4.staticflickr.com/3282/2770447094_2c64348643.jpg'},
-    {name: 'Big Peak', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg'}
-];
+//MongoDB Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
+//INDEX route - show all campgrunds
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds: campArr});
+    //Retrieve campgrounds from database and display
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Retrieving All Campgrounds: ", allCampgrounds);
+            res.render('index', {campgrounds: allCampgrounds});
+        }
+    });
 });
 
+//CREATE route - create new campground
 app.post('/campgrounds', (req, res) => {
-    //Get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campArr.push(newCampground);
-    res.redirect('/campgrounds');
+    //Get data from form and add to database Campgrounds collection
+    Campground.create({
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    }, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("New Campground: ", campground);
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
+//NEW route - show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
-    res.render('new')
+    res.render('new');
 });
+
+//SHOW route - show info of a campground
+app.get('/campgrounds/:id', (req, res) => {
+    //find campground with provided ID
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('show', {campground: foundCampground});
+        }
+    });
+});
+
 
 app.listen(3000);
 console.log('Server is running on port 3000');
